2737d2588a79e164a2949596176173ec0a173c78
danielgppa <95982867+danielgppa@users.noreply.github.com>
2023-12-26 19:22:49 -0300

    Broadcast Chain

--- pubsub.js
+++ pubsub.js
@@ -8,30 +8,58 @@ const credentials = {
 };
 
 const CHANNELS = {
-  TEST: 'TEST'
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN'
 };
 
 class PubSub {
-  constructor() {
-    this.pubnub = new PubNub(credentials);
+  constructor({ blockchain }) {
+    this.blockchain = blockchain;
 
-    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+    this.publisher = new PubNub(credentials);
+    this.subscriber = new PubNub(credentials);
 
-    this.pubnub.addListener(this.listener());
+    this.subscribeToChannels();
+
+    this.subscriber.addListener({
+      message: messageObject => this.handleMessage(messageObject)
+    });
   }
 
-  listener() {
-    return {
-      message: messageObject => {
-        const { channel, message } = messageObject;
+  handleMessage(channel, message) {
+    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
 
-        console.log(`Message received. Channel: ${channel}. Message: ${message}`);
-      }
-    };
+    const parsedMessage = JSON.parse(message);
+
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      this.blockchain.replaceChain(parsedMessage);
+    }
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach(channel => {
+      this.subscriber.subscribe(channel);
+    });
   }
 
   publish({ channel, message }) {
-    this.pubnub.publish({ channel, message });
+    this.publisher.publish({
+      channel: channel,
+      message: message
+    }, (status, response) => {
+      if (status.error) {
+        console.log(`Publish Error: ${status.message}`);
+      } else {
+        console.log(`Message Published to Channel ${channel} at ${response.timetoken}`);
+      }
+    });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
   }
 }
 
