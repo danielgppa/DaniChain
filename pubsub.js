71a8fb4e27e60449eaa1bea26fc293b09b558edb
danielgppa <95982867+danielgppa@users.noreply.github.com>
2023-12-26 21:40:56 -0300

    Update package.json and pubsub.js

--- pubsub.js
+++ pubsub.js
@@ -9,56 +9,88 @@ const credentials = {
 
 const CHANNELS = {
   TEST: 'TEST',
-  BLOCKCHAIN: 'BLOCKCHAIN'
+  BLOCKCHAIN: 'BLOCKCHAIN',
+  TRANSACTION: 'TRANSACTION'
 };
 
 class PubSub {
-  constructor({ blockchain }) {
+  constructor({ blockchain, transactionPool, wallet }) {
     this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
 
-    this.publisher = new PubNub(credentials);
-    this.subscriber = new PubNub(credentials);
+    this.pubnub = new PubNub(credentials);
 
-    this.subscribeToChannels();
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
 
-    this.subscriber.addListener({
-      message: messageObject => this.handleMessage(messageObject)
-    });
-  }
-
-  handleMessage(channel, message) {
-    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
-
-    const parsedMessage = JSON.parse(message);
-
-    if (channel === CHANNELS.BLOCKCHAIN) {
-      this.blockchain.replaceChain(parsedMessage);
-    }
-  }
-
-  subscribeToChannels() {
-    Object.values(CHANNELS).forEach(channel => {
-      this.subscriber.subscribe(channel);
-    });
-  }
-
-  publish({ channel, message }) {
-    this.publisher.publish({
-      channel: channel,
-      message: message
-    }, (status, response) => {
-      if (status.error) {
-        console.log(`Publish Error: ${status.message}`);
-      } else {
-        console.log(`Message Published to Channel ${channel} at ${response.timetoken}`);
-      }
-    });
+    this.pubnub.addListener(this.listener());
   }
 
   broadcastChain() {
     this.publish({
       channel: CHANNELS.BLOCKCHAIN,
       message: JSON.stringify(this.blockchain.chain)
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction)
+    });
+  }
+
+  subscribeToChannels() {
+    this.pubnub.subscribe({
+      channels: [Object.values(CHANNELS)]
+    });
+  }
+
+  listener() {
+    return {
+      message: messageObject => {
+        const { channel, message } = messageObject;
+
+        console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+        const parsedMessage = JSON.parse(message);
+
+        switch(channel) {
+          case CHANNELS.BLOCKCHAIN:
+            this.blockchain.replaceChain(parsedMessage, true, () => {
+              this.transactionPool.clearBlockchainTransactions(
+                { chain: parsedMessage.chain }
+              );
+            });
+            break;
+          case CHANNELS.TRANSACTION:
+            if (!this.transactionPool.existingTransaction({
+              inputAddress: this.wallet.publicKey
+            })) {
+              this.transactionPool.setTransaction(parsedMessage);
+            }
+            break;
+          default:
+            return;
+        }
+      }
+    }
+  }
+
+  publish({ channel, message }) {
+    this.pubnub.publish({ message, channel });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction)
     });
   }
 }
