00e42fa978d3808433c8e90a7ba088b0c30e5bc3
danielgppa <95982867+danielgppa@users.noreply.github.com>
2024-01-10 21:58:15 -0300

    Created the App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
-render(<div>Hello World!: Fourth Live Update</div>, document.getElementById('root'));
+render(<App/>, document.getElementById('root'));
